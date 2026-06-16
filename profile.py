ec3f2ad249a4c7419ce568419e14936e6b122397
Krow Iroquois <krowiroquois@YorHa-Unit-9S-Remastered.local>
2026-06-16 16:44:57 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,20 @@
+
+full_name = "Krow Sirene Iroquois"
+nickname = "K.I."
+age = 24
+
+used_python_before = False
+
+hobbies = ["writing", "visual art", "video games"]
+
+favorites = {
+    "movie": "Monkeybone",
+    "food": "chicken shawarma",
+    "hobby": "visual art",
+    "manga": "JoJo's Bizarre Adventure: Vento Aureo"}
+
+print(full_name, nickname, age)
+print(used_python_before, hobbies, favorites)
+
+skills = ("leadership", "public speaking")
+languages = {"English", "Some Spanish"}
